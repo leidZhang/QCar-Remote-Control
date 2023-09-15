ba200a27fc000779558578e443b8b925bbe2b5ee
leidZhang <113314347+leidZhang@users.noreply.github.com>
2023-09-15 16:26:56 -0400

    Update serverSocket.py

--- server/serverSocket.py
+++ server/serverSocket.py
@@ -1,9 +1,10 @@
-from socket import * 
+from socket import *
+import pickle  
 
 class ServerSocket: 
     def __init__(self) -> None: 
         self.serverSocket = socket(AF_INET, SOCK_STREAM) 
-        self.hostname = gethostname() 
+        self.hostname = '0.0.0.0' 
         self.port = 8080 
     
     def run(self) -> None: 
@@ -13,17 +14,29 @@ class ServerSocket:
         while True: 
             print("The server is ready to accept information...") 
             connectionSocket, address = self.serverSocket.accept() 
-
             running = True 
+
             while running: 
+                print(f"Connected to {address}")
+
                 try: 
-                    message = connectionSocket.recv(1024).decode() 
-                    if not message: break 
-                    print(f"Got the message from {address}: " + message) 
+                    data = connectionSocket.recv(1024)
+                    if not data: break  
                     
-                    modifiedMessage = message.upper().encode() 
-                    connectionSocket.send(modifiedMessage) 
+                    receivedDict = pickle.loads(data)
+                    # placeholder service
+                    xVal = receivedDict['x'] 
+                    modifiedData = str(xVal)
+                    if xVal < 0: 
+                        modifiedData = 'left: ' + modifiedData 
+                    elif xVal > 0: 
+                        modifiedData = 'right: ' + modifiedData 
+                    else: 
+                        modifiedData = 'center ' + modifiedData 
+                    
+                    connectionSocket.sendall(modifiedData.encode()) 
                 except Exception as e: 
+                    print(e)
                     running = False 
             
             connectionSocket.close() 

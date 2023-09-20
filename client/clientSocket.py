cae83dc9913c77772fc7fc66d125e8dffe5e7479
leidZhang <113314347+leidZhang@users.noreply.github.com>
2023-09-20 15:00:08 -0400

    Add files via upload

--- client/clientSocket.py
+++ client/clientSocket.py
@@ -6,28 +6,41 @@ from utils import handleFullQueue
 class clientSocket: 
     def __init__(self) -> None:
         self.clientSocket = socket(AF_INET, SOCK_STREAM) 
-        self.hostName = '10.0.0.3' 
+        self.hostName = '10.0.0.3' # your QCar ip
         self.port = 8081 
         self.done = False 
 
     def terminate(self): 
         self.done = True 
+        self.clientSocket.close()
         print("Socket stopped")
 
     def run(self, queueLock, dataQueue, responseQueue) -> None: 
-        self.clientSocket.connect((self.hostName, self.port)) 
+        try: 
+            # self.hostName = input("Enter QCar IP address: ")  
+            self.clientSocket.connect((self.hostName, self.port)) 
 
-        while not self.done: 
-            queueLock.acquire() 
+            while not self.done: 
+                queueLock.acquire() 
 
-            if not dataQueue.empty(): 
-                data = dataQueue.get() # get dict object
-                queueLock.release() 
-                self.clientSocket.sendall(pickle.dumps(data)) 
+                try: 
+                    if not dataQueue.empty(): 
+                        data = dataQueue.get() # get dict object
+                        queueLock.release() 
+                        self.clientSocket.sendall(pickle.dumps(data)) 
 
-                response = self.clientSocket.recv(1024)
-                responseData = pickle.loads(response) 
-                handleFullQueue(responseQueue, responseData) 
-            else: 
-                queueLock.release()
+                        response = self.clientSocket.recv(1024)
+                        responseData = pickle.loads(response) 
+                        handleFullQueue(responseQueue, responseData) 
+                    else: 
+                        queueLock.release()
+                except ConnectionResetError:
+                        # Handle the case where the server closes the connection unexpectedly
+                        print("Server connection reset. Reconnecting...")
+                        self.terminate()  # Close the socket
+                        self.clientSocket = socket(AF_INET, SOCK_STREAM)
+                        self.clientSocket.connect((self.hostName, self.port))
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            self.terminate()
                 

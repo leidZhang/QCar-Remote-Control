73d3517586162f4be1b4616991b40db16d9e7bed
leidZhang <113314347+leidZhang@users.noreply.github.com>
2023-09-23 16:30:20 -0400

    Update clientSocket.py

--- client/clientSocket.py
+++ client/clientSocket.py
@@ -10,7 +10,7 @@ class clientSocket:
         self.port = 8081 
         self.done = False 
 
-    def terminate(self): 
+    def terminate(self) -> None: 
         self.done = True 
         self.clientSocket.close()
         print("Socket stopped")
